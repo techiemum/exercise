9d497da4a61e49c951da4be23b1422997320504e
Asmaa Syed <techiemum@workmail.com>
2019-11-09 15:03:04 +1000

    added sum, subtract, devide, get_user_input

--- exercise.rb
+++ exercise.rb
@@ -1,38 +1,66 @@
-
-
-
-quit = false
-
-until quit do
-    puts "options"
-    puts "1.Sum"
-    puts "2.Subtract"
-    puts "3.divide"
-    puts "4.multiply"
-    puts "5.quit"
+# I want to create a calculator that I can access in the command line
+# The calculator should give me a list of options I can do and the choice to quit
+# The calculator should take two values sum, subtract, divide, multiply, square (exponent) them.
+# The calculator should take floats and integers, and strings.
+# The calculator should also take a list of values and add, subtract, divide and multiply them together.
+# The calculator's square function should now work with all Exponents
+# Log each calculation and its result into a text file
+def get_user_input
+    puts "Whats your first number?"
+    num1 = gets.chomp.to_i
+    puts "What's your second number?"
+    num2 = gets.chomp.to_i
+    return num1, num2
+  end
+  def sum
+    num1, num2 = get_user_input()
+    result = num1 + num2
+    puts result
+  end
+  def subtract
+    num1, num2 = get_user_input()
+    result = num1 - num2
+    puts result
+  end
+  quit = false
+  until quit do
+    puts "Options"
+    puts "1. Sum"
+    puts "2. Subtract"
+    puts "3. Divide"
+    puts "4. Multiply"
+    puts "5. Square"
+    puts "q. Quit"
     user_input = gets.chomp
     case user_input
     when "1"
-        puts "what's your first number?"
-        num1 = gets.chomp.to_i
-        puts "what's your second number?"
-        num2 = gets.chomp.to_i
-        result = num1 + num2
-        puts result
+      sum()
+      result = num1 + num2
+      puts result
+    when "2"
+      get_user_input()
+      result = num1 - num2
+      puts result
+    when "3"
+      puts "Whats your first number?"
+      num1 = gets.chomp.to_i
+      puts "What's your second number?"
+      num2 = gets.chomp.to_i
+      result = num1 / num2
+      puts result
+    when "4"
+      puts "Whats your first number?"
+      num1 = gets.chomp.to_i
+      puts "What's your second number?"
+      num2 = gets.chomp.to_i
+      result = num1 * num2
+      puts result
     when "q"
-        quit = true
+      quit = true
     end
-    
-    
-end
-
-# Requirements
-# print options
-# be able to quit
-# takes two values:
-#     sum
-#     subtract
-#     divide 
-#     multiply
-#     square
-
+  end
+  # Requirements 
+  #   takes to values:
+  #     divide
+  #     multiply
+  #     square
