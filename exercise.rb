afee5f8106581af6d8e900f3298f0d0a31044805
Asmaa Syed <techiemum@workmail.com>
2019-11-09 14:44:05 +1000

    added ability to add two values

--- exercise.rb
+++ exercise.rb
@@ -12,6 +12,13 @@ until quit do
     puts "5.quit"
     user_input = gets.chomp
     case user_input
+    when "1"
+        puts "what's your first number?"
+        num1 = gets.chomp.to_i
+        puts "what's your second number?"
+        num2 = gets.chomp.to_i
+        result = num1 + num2
+        puts result
     when "q"
         quit = true
     end
