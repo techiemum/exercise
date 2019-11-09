3886152b42e0308690ecae17f2543c7b888f57bc
Asmaa Syed <techiemum@workmail.com>
2019-11-09 14:38:26 +1000

    quit the loop

--- exercise.rb
+++ exercise.rb
@@ -1,11 +1,22 @@
-loop do
+
+
+
+quit = false
+
+until quit do
     puts "options"
     puts "1.Sum"
     puts "2.Subtract"
     puts "3.divide"
     puts "4.multiply"
     puts "5.quit"
-
+    user_input = gets.chomp
+    case user_input
+    when "q"
+        quit = true
+    end
+    
+    
 end
 
 # Requirements
