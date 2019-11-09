82878651338b78a430b5453ca6eca3813b305284
Asmaa Syed <techiemum@workmail.com>
2019-11-09 14:31:08 +1000

    added option to user

---
+++ exercise.rb
@@ -0,0 +1,20 @@
+loop do
+    puts "options"
+    puts "1.Sum"
+    puts "2.Subtract"
+    puts "3.divide"
+    puts "4.multiply"
+    puts "5.quit"
+
+end
+
+# Requirements
+# print options
+# be able to quit
+# takes two values:
+#     sum
+#     subtract
+#     divide 
+#     multiply
+#     square
+
